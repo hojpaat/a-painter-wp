4379d7a8438924725ede4ce249782ff5f73f74ed
hojpaat <judit.of.horvath@gmail.com>
2025-01-12 23:54:02 +0000

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import './App.css'
-import ContactsPage from './components/ContactsPage';
-import HomePage from './components/HomePage';
 import NavBar from './components/NavBar';
-import { Outlet, Route, Routes } from 'react-router';
-import ReviewsPage from './components/ReviewsPage';
-import ServicesPage from './components/ServicesPage';
+import { Outlet } from 'react-router';
 
 function App() {
 
