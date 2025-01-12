422a29798a39c39d9d6308ee490358867fcc8d9c
hojpaat <judit.of.horvath@gmail.com>
2025-01-12 20:21:12 +0000

    Wrapp App in BrowserRouter for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
+const root = document.getElementById('root')!;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
+
 )
+
