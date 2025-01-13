297394f4496199c0fee8c912070d91a6519395f9
hojpaat <judit.of.horvath@gmail.com>
2025-01-13 20:20:53 +0000

    Create Gallery folder and ImageTile page

---
+++ src/components/Gallery/GalleryCard.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import ImageTile from "./ImageTile";
+
+const GalleryCard: FC = () => {
+    return (
+        <div>
+            This is a gallery card
+            <ImageTile />
+        </div>
+    )
+}
+
+export default GalleryCard;
---
+++ src/components/Gallery/ImageTile.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const ImageTile: FC = () => {
+    return (
+        <div>
+            Image Tile
+        </div>
+    )
+};
+
+export default ImageTile;
