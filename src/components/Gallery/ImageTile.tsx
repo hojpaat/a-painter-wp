0da97f98010aabe2cfe3baa12cf2550fad45aca4
hojpaat <judit.of.horvath@gmail.com>
2025-01-18 14:03:34 +0000

    Add ImageTile styling

--- src/components/Gallery/ImageTile.tsx
+++ src/components/Gallery/ImageTile.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
+import "./ImageTile.scss";
 
 const ImageTile: FC = () => {
     return (
-        <div>
-            Image Tile
-        </div>
+        <>
+
+            <div className="imageTile">
+                <img className="imageTilePic" src="https://frscott.co.uk/wp-content/uploads/2023/03/iStock-1372374195-scaled.jpg" alt='picture of a door' />
+            </div>
+            <div className="imageTile alt">
+                <img className="imageTilePic" src="https://frscott.co.uk/wp-content/uploads/2023/03/iStock-1372374195-scaled.jpg" alt='picture of a door' />
+            </div>
+        </>
     )
 };
 
