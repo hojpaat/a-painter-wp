c8e6b99715a1335dd97f4576af6c4040ba011253
hojpaat <judit.of.horvath@gmail.com>
2025-01-18 14:07:10 +0000

    Update ImageTile styling

--- src/components/Gallery/ImageTile.tsx
+++ src/components/Gallery/ImageTile.tsx
@@ -3,15 +3,9 @@ import "./ImageTile.scss";
 
 const ImageTile: FC = () => {
     return (
-        <>
-
-            <div className="imageTile">
-                <img className="imageTilePic" src="https://frscott.co.uk/wp-content/uploads/2023/03/iStock-1372374195-scaled.jpg" alt='picture of a door' />
-            </div>
-            <div className="imageTile alt">
-                <img className="imageTilePic" src="https://frscott.co.uk/wp-content/uploads/2023/03/iStock-1372374195-scaled.jpg" alt='picture of a door' />
-            </div>
-        </>
+        <div className="imageTile">
+            <img className="imageTilePic" src="https://frscott.co.uk/wp-content/uploads/2023/03/iStock-1372374195-scaled.jpg" alt='picture of a door' />
+        </div>
     )
 };
 
